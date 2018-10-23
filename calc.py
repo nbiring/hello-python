62ef07c4f0438a3796133c146f9e0a6e9f45146a
nbiring <navnitbiring@yahoo.com>
2018-10-23 15:41:09 -0700

    Removed whitespaces

--- calc.py
+++ calc.py
@@ -9,11 +9,11 @@ def multiply(a, b):
 def divide(a, b):
 	return a / b
 
+
 def subtract(a, b):
 	return a - b
+
 
 print('2 + 3 = %d' % add(2, 3))
 print('2 * 5 = %d' % multiply(2, 5))
 print('5 - 2 = %d' % subtract(5, 2))
-
-

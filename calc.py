e5fdff9e0bcd3788bc5523b451f45fa5ea2cde85
nbiring <navnitbiring@yahoo.com>
2020-03-23 07:25:49 -0700

    Added comments to all functions.

--- calc.py
+++ calc.py
@@ -3,15 +3,15 @@ def add(a, b): # Addition Function
 	return a + b
 
 
-def multiply(a, b):
+def multiply(a, b): # Multipy Function
 	return a * b
 
 
-def divide(a, b):
+def divide(a, b): # Divide Function
 	return a / b
 
 
-def subtract(a, b):
+def subtract(a, b): # Subtract Function
 	return a - b
 
 
